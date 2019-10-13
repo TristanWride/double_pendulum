923d276e857aaf485953c964d28160087e3e7814
Tristan Wride <tristanwride@yahoo.co.uk>
2019-10-13 13:36:48 +0100

    Fix incorrect indenting.

--- javascript/main.js
+++ javascript/main.js
@@ -101,7 +101,7 @@ function pause() {
 
 // Play the simulation.
 function play() {
-	playing = true;
+  playing = true;
   document.getElementById("playPauseButton").innerHTML = "<b>PAUSE</b>";
 }
 
@@ -128,20 +128,20 @@ function clear() {
 }
 
 // Reset the simulation.
-function reset() {	
-	setVars();
-	
-	pause();
+function reset() {  
+  setVars();
+  
+  pause();
 
-	clear();
+  clear();
 
-	// Normalise angles to a value between -PI and +PI.
-	normaliseAngles();
+  // Normalise angles to a value between -PI and +PI.
+  normaliseAngles();
 
-	// Add the new start value to the array of t2 values 
-	// (for the Angle 2 against time graph)
+  // Add the new start value to the array of t2 values 
+  // (for the Angle 2 against time graph)
   t2Data = [t2];
-  totalTime = 0;	
+  totalTime = 0;  
 }
 
 // Normalise an angle to one between -PI and +PI.
@@ -154,13 +154,13 @@ function normalise(x) {
     x += 2 * Math.PI;
   }
 
-	return x;
+  return x;
 }
 
 // Normalise the angles to a value between -PI and +PI
 function normaliseAngles() {
-	t1 = normalise(t1);
-	t2 = normalise(t2);
+  t1 = normalise(t1);
+  t2 = normalise(t2);
 }
 
 // Function for the subspace used for graphing the Poincare map.
@@ -168,22 +168,22 @@ function normaliseAngles() {
 // should return a different value in each of these sections of the phase
 // space.
 function section(a, b) {
-	return a >= b;
+  return a >= b;
 }
 
 // Update the pendulum canvas display.
 function drawPendulum() {
-	// Convert the x, y values of the pendulum masses to pixel values for the
-	// canvas.
+  // Convert the x, y values of the pendulum masses to pixel values for the
+  // canvas.
   var p1x = canvas.width / 2 + r1 * m2px * Math.sin(t1);
   var p1y = canvas.height / 2 + r1 * m2px * Math.cos(t1);
   
   var p2x = p1x + r2 * m2px * Math.sin(t2);
   var p2y = p1y + r2 * m2px * Math.cos(t2);
 
-	var pendulumMassSize = 10;
-	
-	// Draw the pendulum rods.
+  var pendulumMassSize = 10;
+  
+  // Draw the pendulum rods.
   ctx.beginPath();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.moveTo(canvas.width / 2, canvas.height / 2);
@@ -191,21 +191,21 @@ function drawPendulum() {
   ctx.lineTo(p2x, p2y);
   ctx.stroke();
   
-	// Draw the pendulum masses.
-	ctx.fillRect(p1x - pendulumMassSize / 2, p1y - pendulumMassSize / 2,
-		pendulumMassSize, pendulumMassSize);
-	ctx.fillRect(p2x - pendulumMassSize / 2, p2y - pendulumMassSize / 2,
-		pendulumMassSize, pendulumMassSize);
+  // Draw the pendulum masses.
+  ctx.fillRect(p1x - pendulumMassSize / 2, p1y - pendulumMassSize / 2,
+    pendulumMassSize, pendulumMassSize);
+  ctx.fillRect(p2x - pendulumMassSize / 2, p2y - pendulumMassSize / 2,
+    pendulumMassSize, pendulumMassSize);
 }
 
 // Plot a dot on the t1 vs t2 graph.
 function drawt1vst2Graph() {
-	// Calculate pixel location of dot.
-	var xLoc = graph.width / 2 + t1/(2 * Math.PI) * graph.width;
-	var yLoc = graph.height / 2 - t2/(2 * Math.PI) * graph.height;
-	var dotSize = 2;
+  // Calculate pixel location of dot.
+  var xLoc = graph.width / 2 + t1/(2 * Math.PI) * graph.width;
+  var yLoc = graph.height / 2 - t2/(2 * Math.PI) * graph.height;
+  var dotSize = 2;
 
-	// Plot dot.
+  // Plot dot.
   gctx.fillRect(xLoc - dotSize / 2, yLoc - dotSize / 2, dotSize, dotSize);
 }
 
@@ -213,82 +213,82 @@ function drawt1vst2Graph() {
 // Similar to the t1 vs t2 graph, except only points are plotted when the
 // system passes through a subspace in the phase space.
 function poincarePlot() {
-	// Plot a point on the poincare map if necessary.
+  // Plot a point on the poincare map if necessary.
   if (mapPlot) {
-		// Calculate pixel location of dot.
-		var xLoc = map.width / 2 + t1/(2 * Math.PI) * map.width;
-		var yLoc = map.height / 2 - t2/(2 * Math.PI) * graph.height;
-		var dotSize = 2;
+    // Calculate pixel location of dot.
+    var xLoc = map.width / 2 + t1/(2 * Math.PI) * map.width;
+    var yLoc = map.height / 2 - t2/(2 * Math.PI) * graph.height;
+    var dotSize = 2;
 
-		// Plot dot.
-		mctx.fillRect(xLoc - dotSize / 2, yLoc - dotSize / 2, dotSize, dotSize);
-  	mapPlot = false;	
+    // Plot dot.
+    mctx.fillRect(xLoc - dotSize / 2, yLoc - dotSize / 2, dotSize, dotSize);
+    mapPlot = false;  
   }
 }
 
 // Draw the line for the angle 2 against time graph.
 function drawTimeGraph() {
-	// Plot on the angle 2 against time graph.
+  // Plot on the angle 2 against time graph.
   tctx.beginPath();
 
-	// Clear canvas first.
+  // Clear canvas first.
   tctx.clearRect(0, 0, timeGraph.width, timeGraph.height);
-	
-	// Plot values stored in t2Data array.
-	tctx.moveTo(timeGraph.width, timeGraph.height / 2 - 
-		t2Data[t2Data.length - 1] / (2 * Math.PI) * timeGraph.height);
-	
+  
+  // Plot values stored in t2Data array.
+  tctx.moveTo(timeGraph.width, timeGraph.height / 2 - 
+    t2Data[t2Data.length - 1] / (2 * Math.PI) * timeGraph.height);
+  
   for (var i = 1; i < t2Data.length; i++) {
     if (Math.abs(t2Data[t2Data.length - 1 - i] - 
-					t2Data[t2Data.length - i]) < 3) 
-		{
-			// Draw a continuous line.
+          t2Data[t2Data.length - i]) < 3) 
+    {
+      // Draw a continuous line.
       tctx.lineTo(timeGraph.width - i, timeGraph.height / 2 - 
-				t2Data[t2Data.length - 1 - i] / (2 * Math.PI) * timeGraph.height);
+        t2Data[t2Data.length - 1 - i] / (2 * Math.PI) * timeGraph.height);
     } else {
-			// Value has wrapped around (exceeded +PI or dropped below -PI).
-			// Start a new line.
+      // Value has wrapped around (exceeded +PI or dropped below -PI).
+      // Start a new line.
       tctx.moveTo(timeGraph.width - i, timeGraph.height / 2 - 
-				t2Data[t2Data.length - 1 - i] / (2 * Math.PI) * timeGraph.height);	
+        t2Data[t2Data.length - 1 - i] / (2 * Math.PI) * timeGraph.height);  
     }
   }
 
-  tctx.stroke();	
+  tctx.stroke();  
 }
 
 // Apply the Runge-Kutta method to the system.
 // This is a method for numerically solving differential equations.
 function applyRungeKutta() {
-	// Set variables for the Runge-Kutta method.
-	// k[x][0] for 0 <= x <= 3 is used for the calculation of the new t1 value
-	// k[x][1] for 0 <= x <= 3 is used for the calculation of the new t2 value
-	// k[x][2] for 0 <= x <= 3 is used for the calculation of the new dt1 value
-	// k[x][3] for 0 <= x <= 3 is used for the calculation of the new dt2 value
+  // Set variables for the Runge-Kutta method.
+  // k[x][0] for 0 <= x <= 3 is used for the calculation of the new t1 value
+  // k[x][1] for 0 <= x <= 3 is used for the calculation of the new t2 value
+  // k[x][2] for 0 <= x <= 3 is used for the calculation of the new dt1 value
+  // k[x][3] for 0 <= x <= 3 is used for the calculation of the new dt2 value
   k[0][0] = h * dt1;
   k[0][1] = h * dt2;
   k[0][2] = h * ddt1(t1, t2, dt1, dt2);
   k[0][3] = h * ddt2(t1, t2, dt1, dt2);
-	
+  
   k[1][0] = h * (dt1 + 0.5 * k[0][2]);
   k[1][1] = h * (dt2 + 0.5 * k[0][3]);
-	k[1][2] = h * ddt1(t1 + 0.5 * k[0][0], t2 + 0.5 * k[0][1], dt1 + 0.5 *
-		k[0][2], dt2 + 0.5 * k[0][3]);
-	k[1][3] = h * ddt2(t1 + 0.5 * k[0][0], t2 + 0.5 * k[0][1], dt1 + 0.5 *
-		k[0][2], dt2 + 0.5 * k[0][3]);
+  k[1][2] = h * ddt1(t1 + 0.5 * k[0][0], t2 + 0.5 * k[0][1], dt1 + 0.5 *
+    k[0][2], dt2 + 0.5 * k[0][3]);
+  k[1][3] = h * ddt2(t1 + 0.5 * k[0][0], t2 + 0.5 * k[0][1], dt1 + 0.5 *
+    k[0][2], dt2 + 0.5 * k[0][3]);
 
   k[2][0] = h * (dt1 + 0.5 * k[1][2]);
   k[2][1] = h * (dt2 + 0.5 * k[1][3]);
-	k[2][2] = h * ddt1(t1 + 0.5 * k[1][0], t2 + 0.5 * k[1][1], dt1 + 0.5 *
-		k[1][2], dt2 + 0.5 * k[1][3]);
-	k[2][3] = h * ddt2(t1 + 0.5 * k[1][0], t2 + 0.5 * k[1][1], dt1 + 0.5 *
-		k[1][2], dt2 + 0.5 * k[1][3]);
+  k[2][2] = h * ddt1(t1 + 0.5 * k[1][0], t2 + 0.5 * k[1][1], dt1 + 0.5 *
+    k[1][2], dt2 + 0.5 * k[1][3]);
+  k[2][3] = h * ddt2(t1 + 0.5 * k[1][0], t2 + 0.5 * k[1][1], dt1 + 0.5 *
+    k[1][2], dt2 + 0.5 * k[1][3]);
 
   k[3][0] = h * (dt1 + k[2][2]);
   k[3][1] = h * (dt2 + k[2][3]);
-	k[3][2] = h * ddt1(t1 + k[2][0], t2 + k[2][1], dt1 + k[2][2], dt2 + k[2][3]);
-  k[3][3] = h * ddt2(t1 + k[2][0], t2 + k[2][1], dt1 + k[2][2], dt2 + k[2][3]);	
-	
-	// Apply the Runge-Kutta method.
+  k[3][2] = h * ddt1(t1 + k[2][0], t2 + k[2][1], dt1 + k[2][2], dt2 + k[2][3]);
+  k[3][3] = h * ddt2(t1 + k[2][0], t2 + k[2][1], dt1 + k[2][2], dt2 + k[2][3]);  
+  
+  // Apply the Runge-Kutta method.
   t1 += k[0][0]/6 + k[1][0]/3 + k[2][0]/3 + k[3][0]/6;
   t2 += k[0][1]/6 + k[1][1]/3 + k[2][1]/3 + k[3][1]/6;
   dt1 += k[0][2]/6 + k[1][2]/3 + k[2][2]/3 + k[3][2]/6;
@@ -296,58 +296,58 @@ function applyRungeKutta() {
 }
 
 // Simulate one time step.
-function update() {	
+function update() {  
   var before = section(dt1, dt2);
-	
-	// Apply the Runge-Kutta method to increment the pendulum's variables
-	// according to Newtonian mechanics.
-	applyRungeKutta();
-	
-	// If output from the section function has changed, a point must be plotted
-	// on the Poincare map.
+  
+  // Apply the Runge-Kutta method to increment the pendulum's variables
+  // according to Newtonian mechanics.
+  applyRungeKutta();
+  
+  // If output from the section function has changed, a point must be plotted
+  // on the Poincare map.
   if(before != section(dt1, dt2)) {
     mapPlot = true;
   }
-	
-	// Normalise angles to a value between -PI and +PI
-	normaliseAngles();
-	
-	// Increment time value.
+  
+  // Normalise angles to a value between -PI and +PI
+  normaliseAngles();
+  
+  // Increment time value.
   totalTime += h;
-	// Add data to the array of t2 data.
-  t2Data.push(t2);	
+  // Add data to the array of t2 data.
+  t2Data.push(t2);  
 }       
 
 // Draw to canvasses.
-function render() {	
-	drawPendulum();
+function render() {  
+  drawPendulum();
 
-	drawt1vst2Graph();
-		
-	poincarePlot();
+  drawt1vst2Graph();
+    
+  poincarePlot();
 
-	drawTimeGraph();
+  drawTimeGraph();
 }
 
 function playButtonPressed() {
-	// Update the text displayed by the play/pause button.
+  // Update the text displayed by the play/pause button.
   if (playing) {
-		pause();
+    pause();
   } else {
-		play();
+    play();
   }
 }
 
 // Main loop for the canvasses.
-function mainLoop() {	
-	if(playing) {
-		// Update variables.
+function mainLoop() {  
+  if(playing) {
+    // Update variables.
     update();
   }
 
-	// Draw to canvasses.
+  // Draw to canvasses.
   render();
 
-	// Repeat main loop.
-  requestAnimationFrame(mainLoop);	
+  // Repeat main loop.
+  requestAnimationFrame(mainLoop);  
 }
